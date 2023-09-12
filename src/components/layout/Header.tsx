838b05e1c6cd8e14e95f41c0722bfe65a70e8d93
Matheus Torres <matheustorres1201@gmail.com>
2023-09-12 11:07:40 -0400

    update header

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,9 @@
+"use client";
+import { styled } from "styled-components";
+
+export const Header = styled.div`
+    background-image: url("/logo-satus.png");
+    width: 150px;
+    height: 50px;
+    background-size: contain;
+`;
