febee9450d9f9de8c1c1fdc80fb948cee6308393
Giovanne Feitosa <giovanneafonso@gmail.com>
2023-09-19 19:55:20 -0300

    make footer component look better

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -5,28 +5,14 @@ export function Footer (){
 		
 <footer className="bg-white  shadow dark:bg-gray-900">
     <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <div className="sm:flex sm:items-center sm:justify-between">
-            <a href="#" className="flex items-center mb-4 sm:mb-0">
-                <img src="/logo-satus.png" className="h-8 mr-3" alt="Flowbite Logo" />
-                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white" />
+        <div className="flex justify-center">
+            <a href="#" className="">
+                <img src="/logo-satus.png" className="h-12" alt="Projeto Satus" />
             </a>
-            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-                <li>
-                    <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
-                </li>
-                <li>
-                    <a href="#" className="mr-4 hover:underline md:mr-6">Privacy Policy</a>
-                </li>
-                <li>
-                    <a href="#" className="mr-4 hover:underline md:mr-6 ">Licensing</a>
-                </li>
-                <li>
-                    <a href="#" className="hover:underline">Contact</a>
-                </li>
-            </ul>
         </div>
-        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="#" className="hover:underline">Satus™</a>. All Rights Reserved.</span>
+        <div className="mt-10 text-sm text-gray-500 text-center dark:text-gray-400">
+          © 2023 Projeto Satus™
+        </div>
     </div>
 </footer>
 
